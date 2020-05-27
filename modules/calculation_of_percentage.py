b567eec693c625144279ead6150ca0260249d5fe
Kozorez-V <kozorezviktoria@gmail.com>
2020-05-27 09:25:09 +0400

    validation was added

--- modules/calculation_of_percentage.py
+++ modules/calculation_of_percentage.py
@@ -1,5 +1,8 @@
 def calculation(count_people):
     result = int(count_people["count_selected_country"]) * 100 / int(count_people["count_all_countries"])
+    if result < 0:
+        print("Result must be positive")
+        exit()
     return result
 
 def show_calculation_result(result):
